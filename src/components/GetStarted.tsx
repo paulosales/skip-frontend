6aaf43377a4cc74d36419f301d71a0e57dae1667
Paulo Rogério Sales Santos <paulosales@gmail.com>
2020-07-04 00:11:08 -0300

    Build fixes

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import styled from "styled-components";
 import Button from "./Button";
 
--- src/components/Offers.tsx
+++ src/components/Offers.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import styled from "styled-components";
 
 const OffersContainer = styled.section`
